35a0c03cb11e4f9c50c10e9723268ac08ff55d5e
supran2811 <supransengupta@gmail.com>
2020-08-31 05:38:58 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export * from './constants/app-constant';
 export * from './decorators';
 export * from './errors';
-export * from './middlewares';
+// export * from './middlewares';
+export * from './middlewares/currentUser';
+export * from './middlewares/error-handler';
+export * from './middlewares/validateRequest';
