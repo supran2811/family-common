a997ac0bdef3b0845a0c70d51835d40f88408093
supran2811 <supransengupta@gmail.com>
2020-09-08 06:05:16 +0530

    update

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -2,3 +2,4 @@ export * from './controller';
 export * from './routeBinders';
 export * from './validator';
 export * from './useMiddleware';
+export * from './usePreValidateMiddleware';
--- src/decorators/usePreValidateMiddleware.ts
+++ src/decorators/usePreValidateMiddleware.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from 'express';
 import 'reflect-metadata';
 import { MetadataKeys } from './constants';
 
-export function useMiddleware(middleware: RequestHandler) {
+export function usePreValidateMiddleware(middleware: RequestHandler) {
   return function (target: any, key: string) {
     const middlewares =
       Reflect.getMetadata(MetadataKeys.PREVALIDATE_MIDDLEWARE, target, key) ||
